63862be38b8a25431758f5765eac61a00b7e2e25
lucas gois <fucoieemc@gmail.com>
2021-06-21 23:53:18 -0300

    campaign feature

--- seller_product_api-master/controllers/campaigns.js
+++ seller_product_api-master/controllers/campaigns.js
@@ -1,54 +1,56 @@
-const Campaign = require('../models/campaigns')
-const campaign = require('../repositories/campaign')
+const Campaign = require("../models/campaigns");
+const { findItems } = require("../repositories/campaign");
+const campaign = require("../repositories/campaign");
 
-module.exports = app => {
-    
-    app.delete("/campaign/:id", (req, res) => {
-        id = req.params.id
+module.exports = (app) => {
+  app.delete("/campaign/:id", (req, res) => {
+    id = req.params.id;
 
-        Campaign.delete(id)
-        .then(res.status(204).json() 
-        ).catch(err => res.status(400).json(err))
-    })
+    Campaign.delete(id)
+      .then(res.status(204).json())
+      .catch((err) => res.status(400).json(err));
+  });
 
-    app.post("/campaign/delete/:id", (req, res) => {
-        id = req.params.id
+  app.post("/campaign/delete/:id", (req, res) => {
+    id = req.params.id;
 
-        Campaign.delete(id)
-        .then(res.status(204).json() 
-        ).catch(err => res.status(400).json(err))
-    })
+    Campaign.delete(id)
+      .then(res.status(204).json())
+      .catch((err) => res.status(400).json(err));
+  });
 
-    app.get("/campaign/:id", (req, res) => {
-        id = req.params.id
+  app.get("/campaign/:id", (req, res) => {
+    id = req.params.id;
 
-        Campaign.findOne(id)
-        .then(response =>{
-            response[0].items = JSON.parse(response[0].items)
-            res.status(200).json(response[0])
-        }
-        ).catch(err => res.status(400).json(err))
-    })
+    Campaign.findOne(id)
+      .then((response) => {
+        response[0].items = JSON.parse(response[0].items);
+        res.status(200).json(response[0]);
+      })
+      .catch((err) => res.status(400).json(err));
+  });
 
-    app.route("/campaigns")
+  app
+    .route("/campaigns")
 
     .post((req, res) => {
-        camp = req.body
-        Campaign.add(camp)
-        .then(newCampaign => {
-
-            res.status(201).json(newCampaign)
-        }).catch(err => res.status(400).json(err))
+      camp = req.body;
+      Campaign.add(camp)
+        .then((newCampaign) => {
+          res.status(201).json(newCampaign);
+        })
+        .catch((err) => res.status(400).json(err));
     })
 
     .get((req, res) => {
-        Campaign.listAll()
-        .then(response => {
-            response.forEach(response => response.items = (JSON.parse(response.items)))
+      Campaign.listAll()
+        .then((response) => {
+          response.forEach(
+            (response) => (response.items = JSON.parse(response.items))
+          );
 
-            res.status(200).json(response)
-        }).catch(err => res.status(400).json(err))
-    })
-
-    
-}
+          res.status(200).json(response);
+        })
+        .catch((err) => res.status(400).json(err));
+    });
+};
